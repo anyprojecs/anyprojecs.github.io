b8b0a606e4f81d62172f0258cd6b0298def75c53
dexiii <68478077+dexiii00@users.noreply.github.com>
2024-12-03 22:50:16 +0100

    Update loans.js

--- stoneslots/loans.js
+++ stoneslots/loans.js
@@ -1,10 +1,19 @@
 let loan = 0;
 
-function tookLoan() {
+function tookLoan(takenLoan) {
     if (loan == 0) {
-        money += 1200;
-        updateMoney();
-        loan += 14
+        if (takenLoan == 1200){
+            money += 1200;
+            updateMoney();
+            loan += 14
+            loanInterest = 100;
+        }
+        else if (takenLoan == 5400) {
+            money += 5400;
+            updateMoney();
+            loan += 28
+            loanInterest = 200;
+        }
     }
     else{
         showError("You already have an active loan!")
@@ -17,7 +26,7 @@ function payLoan() {
             showError("You can't pay a loan!");
         }
         else {
-            money -= 100;
+            money -= loanInterest;
             loan -= 1;
             console.log(loan);
         }
